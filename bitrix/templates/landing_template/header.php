2c3f858ee6aea514c83171a37f6490a1bc9fa006
elCortez <evgenylebedevv@gmail.com>
2024-02-12 18:45:16 +0300

    Update header.php

--- bitrix/templates/landing_template/header.php
+++ bitrix/templates/landing_template/header.php
@@ -1,1 +1,1 @@
-<? $myMess = GetMessage("ABOUT_US"); echo $myMess; ?> //recieve message from (template/lang/ru/header.php) language file to this main file header.php
+<? echo GetMessage("ABOUT_US"); ?> //recieve message from (template/lang/ru/header.php) language file to this main file header.php
